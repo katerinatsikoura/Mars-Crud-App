1a00ce95551b827ab1283fd0a26b9ead13bcb357
katerinatsikoura <147326236+katerinatsikoura@users.noreply.github.com>
2025-04-01 21:56:59 +0300

    Resource creation

---
+++ src/main/java/com/marsdeployment/marscrud/Resource.java
@@ -0,0 +1,99 @@
+package com.marsdeployment.marscrud.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Resource {
+
+    /**
+    * Resource's id.
+    */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+    * Resource's name.
+    */
+    private String name;
+
+    /**
+    * Resource's quantity.
+    */
+    private int quantity;
+
+    
+    /**
+    * Default constructor declaration.
+    */
+    public Resource() {}
+
+    /**
+    * Core constructor.
+    * 
+    * @param name        The name of the resource
+    * @param quantity    Resource's quantity
+    * 
+    */
+    public Resource(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    /**
+    * Gets the id of the resource.
+    * 
+    * @return The resource id.
+    */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+    * Sets the id of the resource.
+    * 
+    * @param id resource's id
+    */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+    * Gets the resource's name.
+    * 
+    * @return resource's name
+    */
+    public String getName() {
+        return name;
+    }
+
+    /**
+    * Sets the name of the resource.
+    * 
+    * @param name
+    */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+    * Gets the resource's quantity.
+    * 
+    * @return resource's quantity
+    */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+    * Sets the quantity of the resource.
+    * 
+    * @param quantity
+    */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
