516581b9c0e9460669b8b4294a99a06175c34036
katerinatsikoura <147326236+katerinatsikoura@users.noreply.github.com>
2025-04-01 21:22:58 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/marsdeployment/marscrud/MarsCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.marsdeployment.marscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsCrudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marsdeployment/marscrud/MarsCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marsdeployment.marscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
